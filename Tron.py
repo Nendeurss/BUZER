d94820697c152124afe65e3dcdea9d6350701961
unknown <trankevintr@gmail.com>
2020-05-03 15:53:43 +0200

    First version of Tron (final)

--- Tron.py
+++ Tron.py
@@ -35,6 +35,15 @@ GInit = np.flip(GInit, 0).transpose()
 LARGEUR = 13    # nombre de colonnes + 1
 HAUTEUR = 17    # nombre de lignes + 1
 
+dx = np.array([0, -1, 0,  1,  0],dtype=np.int8)
+dy = np.array([0,  0, 1,  0, -1],dtype=np.int8)
+
+# scores associés à chaque déplacement
+ds = np.array([0,  1,  1,  1,  1],dtype=np.int8)
+
+Debug = False
+nb = 10000 # nb de parties
+
 # La classe Game nous permet de regrouper dans un même objet :
 # - la grille,
 # - la position du joueur
@@ -213,7 +222,8 @@ def NextPlay(Game):
         Game2.PlayerX = i[0]
         Game2.PlayerY = i[1]
         # On ajoute le score retourné de MonteCarlo dans la liste de score
-        averageScore.append(MonteCarlo(Game2, 1000))
+        # averageScore.append(MonteCarlo(Game2, 1000))
+        averageScore.append(Simulate(Game2))
 
     # On récupère l'indice de la valeur maximale de la liste des scores
     maxScore = averageScore.index(max(averageScore))
@@ -254,6 +264,104 @@ def Play(Game):
     # la partie continue, on retourne False
     return False
 
+def Simulate(Game):
+
+    # on copie les datas de départ pour créer plusieurs parties en //
+    G      = np.tile(Game.Grille,(nb,1,1))
+    X      = np.tile(Game.PlayerX,nb)
+    Y      = np.tile(Game.PlayerY,nb)
+    S      = np.tile(Game.Score,nb)
+    I      = np.arange(nb)  # 0,1,2,3,4,5...
+    boucle = True
+    if Debug : AffGrilles(G,X,Y)
+
+    # VOTRE CODE ICI
+
+    OldScore = -1
+    #On créer le vecteur Vgauche, et on met tout à True
+    Vgauche = np.tile(True,nb)
+    Vdroite = np.tile(True,nb)
+    Vhaut = np.tile(True,nb)
+    Vbas = np.tile(True,nb)
+    #On transforme les True en 1 et les False en 0
+    Vgauche = (Vgauche == 1)*1
+    Vdroite = (Vdroite == 1)*1
+    Vhaut = (Vhaut == 1)*1
+    Vbas = (Vbas == 1)*1
+
+    
+
+    while(boucle) :
+        if Debug :print("X : ",X)
+        if Debug :print("Y : ",Y)
+        if Debug :print("S : ",S)
+
+        # marque le passage de la moto
+        G[I, X, Y] = 2
+
+        #On créer un vecteur de tableau des coups possibles et on met tout à 0
+        LPossibles = np.zeros((nb,4),dtype=np.int8)
+
+        #On créer un vecteur d'indice et on met tout à zero
+        Indices = np.zeros(nb,dtype=np.int8)
+
+        if Debug :print("Indices : ",Indices)
+
+        #Je remplis les vecteurs des valeurs de directions ex: Vgauche = [1,1,1,1,....]; Vhaut = [2,2,2,2,....]; ....
+        Vgauche = (G[I,X-1,Y] == 0)*1
+        Vdroite = (G[I,X+1,Y] == 0)*3
+        Vbas = (G[I,X,Y-1] == 0)*4
+        Vhaut = (G[I,X,Y+1] == 0)*2
+
+        if Debug :print("Vgauche :",Vgauche)
+
+        #J'incrémente Indices si on a add une direction possible
+        #Par exemple si j'ai add 1 dans LPossibles[I,Indices], alors je dois retrouver 1 dans LPossibles[I,Indices]
+        LPossibles[I,Indices] = Vgauche
+        Indices = Indices + (LPossibles[I,Indices] != 0)
+        LPossibles[I,Indices] = Vhaut
+        Indices = Indices + (LPossibles[I,Indices] != 0)
+        LPossibles[I,Indices] = Vdroite
+        Indices = Indices + (LPossibles[I,Indices] != 0)
+        LPossibles[I,Indices] = Vbas
+        Indices = Indices + (LPossibles[I,Indices] != 0)
+
+        Indices[Indices == 0] = 1
+
+        if Debug :print("LPossibles :",LPossibles)
+        if Debug :print("Indices :",Indices)
+
+        if Debug :print("Vgauche : ",Vgauche)
+
+        R = np.random.randint(12,size=nb)
+        if Debug :print("R : ",R)
+        R = R % Indices
+        if Debug :print("R : ",R)
+
+        # Direction : 2 = vers le haut
+        Choix = LPossibles[I,R]
+        Score = ds[Choix]
+
+        #DEPLACEMENT
+        DX = dx[Choix]
+        DY = dy[Choix]
+        if Debug : print("DX : ", DX)
+        if Debug : print("DY : ", DY)
+        X += DX
+        Y += DY
+        S += Score
+
+        NewScore = np.sum(S)
+
+        if(NewScore == OldScore): break
+        OldScore = NewScore 
+
+        #debug
+        if Debug : AffGrilles(G,X,Y)
+        if Debug : time.sleep(1)
+
+    
+    return np.mean(S)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
@@ -270,7 +378,7 @@ def Partie():
         Affiche(CurrentGame)
 
         # Appelle la fonction Partie après une pause de 1000ms
-        Window.after(1000, Partie)
+        Window.after(1, Partie)
     else:
         # Quand le joueur entre en collision avec un obstacle, la partie est terminée, on affiche le score
         AfficheScore(CurrentGame)
